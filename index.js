beac83049d355d7b2faf1947a8c9ff758d929c37
evkos <evkos@pm.me>
2020-01-16 02:33:20 +0200

    create compressing functionality
    create compress algorithm
    
    compress image
    
    correct compress function logiv
    
    create new data
    
    create iamgeModel
    
    create imageParser
    
    correct imageCompressor
    
    update compress method
    
    ad corrections
    
    refactoring parser and compressor
    
    add compresing algorithms
    
    add color pallet
    
    correct folder structure
    
    discuss
    
    parser refactoring
    
    refactoring compressor, add 8bit images support
    
    fix 8bit images issue with pallet
    
    Fixed square and rectangle images, 24 and 8 bits
    
    clean code in modules

--- index.js
+++ index.js
@@ -1,69 +1,29 @@
 const fs = require('fs');
+const ImageParser = require('./src/ImageParser');
+const ImageCompressor = require('./src/ImageCompressor');
 
-/*
-const generateThumbnailAsync = (imagePath) => {
-  fs.readFile(imagePath, function(err, data) {
-    createIndexFile(imagePath, thumbnailFile);
-  });
-};
-*/
+const imageParser = new ImageParser();
+const imageCompressor = new ImageCompressor('top-left', 2);
 
-const compressImage = data => {
+const filePath = 'images/test1.bmp';
 
-  const bitMapFileHeader = {
-    fileType: data.slice(0, 2).toString('ascii'),
-    fileSize: data.slice(2, 6).readUIntLE(0, 4),
-    pixelDataOffset: data.slice(10, 14).readUIntLE(0, 4),
-  };
-
-  const bitMapInfoHeader = {
-    headerSize: data.slice(14, 18).readUIntLE(0, 4),
-    imageWidth: data.slice(18, 22).readIntLE(0, 4),
-    imageHeight: data.slice(22, 26).readIntLE(0, 4),
-    planes: data.slice(26, 28).readUIntLE(0, 2),
-    bitsPerPixel: data.slice(28, 30).readUIntLE(0, 2),
-    compression: data.slice(30, 34).readUIntLE(0, 4),
-    imageSize: data.slice(34, 38).readUIntLE(0, 4),
-    xPixelsPerMeter: data.slice(38, 42).readIntLE(0, 4),
-    yPixelsPerMeter: data.slice(42, 46).readIntLE(0, 4),
-    totalColors: data.slice(46, 50).readUIntLE(0, 4),
-    importantColors: data.slice(50, 54).readUIntLE(0, 4),
-  };
-
-  //const pixelData = data.slice(bitMapFileHeader.pixelDataOffset)
-  const pixelData = data.slice(bitMapFileHeader.pixelDataOffset);
-
-  const pixelArray = [];
-  for (let i = 0; i < bitMapInfoHeader.imageWidth; i++) {
-    pixelArray[i] = []
-    for(let j = 0; j<bitMapInfoHeader.imageHeight; j++){
-      pixelArray[i][j] = 0
-    }
-  }
-
-
-  console.log(pixelArray);
-  return data;
+const generateThumbnail = (filePath) => {
+  const imageObject = imageParser.parseImageFileToImageObject(filePath);
+  const compressedImageObject = imageCompressor.compressImage(imageObject);
+  return imageParser.parseImageBufferToBase64(compressedImageObject);
 };
 
-const generateThumbnail = imagePath => {
-  const data = fs.readFileSync(imagePath);
-  const compressedData = compressImage(data);
-  return Buffer.from(compressedData).toString('base64');
-};
-
-const createIndexFile = (imagePath, thumbnailFile) => {
+const writeToHtml = (imagePath, thumbnailFile) => {
   const indexData = `
-  <img id="image" src="${imagePath}"/>
+  <img id="image" src="../${imagePath}"/>
   <img id="thumbnail" src="data:image/bmp;base64,${thumbnailFile}"/ >
-  <span>${thumbnailFile}</span>
   `;
 
-  fs.writeFile('index.html', indexData, function(err) {
+  fs.writeFile('public/index.html', indexData, function(err) {
     if (err) throw err;
     console.log('File is created successfully.');
   });
 };
 
-const thumbnail = generateThumbnail('test1.bmp');
-createIndexFile('test1.bmp', thumbnail);
+const thumbnail = generateThumbnail(filePath);
+writeToHtml(filePath, thumbnail);
---
+++ src/ImageCompressor.js
@@ -0,0 +1,129 @@
+class ImageCompressor {
+
+  compressingMethod;
+  compressingFactor;
+
+  constructor(method, factor) {
+    this.compressingMethod = method;
+    this.compressingFactor = factor;
+  }
+
+  compressPixels = (pixels, hasPallet = false) => {
+
+    let compressedPixel;
+
+    switch (this.compressingMethod) {
+      case 'top-left':
+        if (hasPallet) {
+          compressedPixel = pixels[0];
+        } else {
+          compressedPixel = {
+            r: pixels[0][0],
+            g: pixels[0][1],
+            b: pixels[0][2],
+          };
+        }
+        break;
+      case 'average':
+        compressedPixel = {
+          r: (pixels[0][0] + pixels[1][0] + pixels[2][0] + pixels[3][0]) / 4,
+          g: (pixels[0][1] + pixels[1][1] + pixels[2][1] + pixels[3][1]) / 4,
+          b: (pixels[0][2] + pixels[1][2] + pixels[2][2] + pixels[3][2]) / 4,
+        };
+        break;
+      default:
+        console.log('Specify compress method');
+        break;
+    }
+
+    return compressedPixel;
+  };
+
+  updatePixels24Bit = (pixelData, bitMapInfoHeader) => {
+    const arrayHeight = bitMapInfoHeader.imageHeight;
+    const arrayImageWidth = bitMapInfoHeader.imageWidth * 3;
+    const arrayWidth = bitMapInfoHeader.imageWidth * 3 + this.getPadding(bitMapInfoHeader.imageWidth * 3);
+
+    const updatedPixelData = [];
+
+    for (let dH = 0; dH < arrayHeight; dH += 2) {
+      for (let dW = 0; dW < arrayImageWidth; dW += 6) {
+        const pixels = [
+          [pixelData[dH * arrayWidth + dW], pixelData[dH * arrayWidth + dW + 1], pixelData[dH * arrayWidth + dW + 2]],
+          [pixelData[dH * arrayWidth + dW + 3], pixelData[dH * arrayWidth + dW + 4], pixelData[dH * arrayWidth + dW + 5]],
+          [pixelData[arrayWidth + dH * arrayWidth + dW], pixelData[arrayWidth + dH * arrayWidth + dW + 1], pixelData[arrayWidth + dH * arrayWidth + dW + 2]],
+          [pixelData[arrayWidth + dH * arrayWidth + dW + 3], pixelData[arrayWidth + dH * arrayWidth + dW + 4], pixelData[arrayWidth + dH * arrayWidth + dW + 5]],
+        ];
+
+        const { r, g, b } = this.compressPixels(pixels);
+        updatedPixelData.push(r, g, b);
+      }
+
+      this.countPadding(updatedPixelData, bitMapInfoHeader, 3);
+    }
+
+    return updatedPixelData;
+  };
+
+  updatePixels8Bit = (pixelData, bitMapInfoHeader) => {
+    const arrayHeight = bitMapInfoHeader.imageHeight;
+    const arrayImageWidth = bitMapInfoHeader.imageWidth;
+    const arrayWidth = bitMapInfoHeader.imageWidth + this.getPadding(bitMapInfoHeader.imageWidth);
+
+    const updatedPixelData = [];
+
+    for (let dH = 0; dH < arrayHeight; dH += 2) {
+      for (let dW = 0; dW < arrayImageWidth; dW += 2) {
+        const pixels = [
+          pixelData[dH * arrayWidth + dW],
+          pixelData[dH * arrayWidth + dW + 1],
+          pixelData[arrayWidth + dH * arrayWidth + dW],
+          pixelData[arrayWidth + dH * arrayWidth + dW + 1],
+        ];
+
+        const compressedPixel = this.compressPixels(pixels, true);
+        updatedPixelData.push(compressedPixel);
+      }
+      this.countPadding(updatedPixelData, bitMapInfoHeader);
+    }
+
+    return updatedPixelData;
+  };
+
+  getPadding = (pixelsPerRow) => {
+    const uncoveredPixels = pixelsPerRow % 4;
+    return uncoveredPixels === 0 ? 0 : 4 - uncoveredPixels;
+  };
+
+  getDeepCopy = (imageObject) => {
+    return JSON.parse(JSON.stringify(imageObject));
+  };
+
+  countPadding = (updatedPixelData, bitMapInfoHeader, factor = 1) => {
+    const padding = this.getPadding(Math.ceil(bitMapInfoHeader.imageWidth / 2) * factor);
+    //TODO make it shorter
+    for (let i = 0; i < padding; i++) {
+      updatedPixelData.push(0);
+    }
+  };
+
+  compressImage = (imageObject) => {
+
+    const { bitMapInfoHeader, colorPallet, pixelData } = imageObject;
+
+    const modifiedObject = this.getDeepCopy(imageObject);
+
+    modifiedObject.pixelData = (!colorPallet) ?
+      this.updatePixels24Bit(pixelData, bitMapInfoHeader) :
+      this.updatePixels8Bit(pixelData, bitMapInfoHeader);
+
+    modifiedObject.bitMapFileHeader.fileSize = modifiedObject.pixelData.length + modifiedObject.bitMapFileHeader.pixelDataOffset;
+    modifiedObject.bitMapInfoHeader.imageSize = modifiedObject.pixelData.length;
+    modifiedObject.bitMapInfoHeader.imageHeight = Math.ceil(bitMapInfoHeader.imageHeight / this.compressingFactor);
+    modifiedObject.bitMapInfoHeader.imageWidth = Math.ceil(bitMapInfoHeader.imageWidth / this.compressingFactor);
+
+    return modifiedObject;
+  };
+}
+
+module.exports = ImageCompressor;
