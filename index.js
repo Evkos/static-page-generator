1b628232527db5677dca6fa6824dc550304b81f5
evkos <evkos@pm.me>
2020-01-26 00:48:56 +0200

    correct images output, create imageSet modee

--- index.js
+++ index.js
@@ -1,33 +1,37 @@
 const fs = require('fs');
-const path = require('path');
 const ImageParser = require('./src/ImageParser');
 const ImageCompressor = require('./src/ImageCompressor');
 
 const imageParser = new ImageParser();
 const imageCompressor = new ImageCompressor('top-left', 2);
 
-const templatesPath = path.resolve(process.env.TEMPLATES_PATH);
+const templatesPath = process.env.TEMPLATES_PATH;
 
-const generateThumbnail = async (thumbnails, image) => {
+const generateThumbnail = async (imageSet, image) => {
   const imageObject = await imageParser.parseImageFileToImageObject(image.src);
   const compressedImageObject = imageCompressor.compressImage(imageObject);
-  thumbnails.push({
-    src: imageParser.parseImageBufferToBase64(compressedImageObject),
-    alt: image.alt,
-  });
+  imageSet.push(
+    image,
+    {
+      type: 'thumbnail',
+      src: imageParser.parseImageBufferToBase64(compressedImageObject),
+      alt: image.alt,
+    },
+  );
 };
 
-const writeToHtml = (name, content, imagePaths, thumbnailFiles) => {
+const writeToHtml = (name, content, imageSet) => {
 
-  const images = imagePaths.reduce((accumulator, imagePath) => {
-    return accumulator.concat(`<img src="../${imagePath.src}" alt="${imagePath.alt}"/>`);
+  const images = imageSet.reduce((result, item) => {
+    if (item.type === 'image') {
+      return result.concat(`<img src="../${item.src}" alt="${item.alt}"/>`);
+    } else {
+      return result.concat(`<img src="data:image/bmp;base64,${item.src}" alt="${item.alt}"/>`);
+    }
+
   }, '');
 
-  const thumbnails = thumbnailFiles.reduce((accumulator, thumbnailFile) => {
-    return accumulator.concat(`<img src="data:image/bmp;base64,${thumbnailFile.src}" alt="${thumbnailFile.alt}"/>`);
-  }, '');
-
-  const indexData = content.replace(/{{(.*)}}/gs, `${images}${thumbnails}`);
+  const indexData = content.replace(/{{(.*)}}/gs, `${images}`);
 
   const resultsFolder = 'public';
 
@@ -48,23 +52,23 @@ const main = () => {
         const contentParts = content.match(/<(.*)>/g);
 
         const images = [];
-        const thumbnails = [];
+        const imageSet = [];
 
         contentParts.forEach((part) => {
           const partArray = part.match(/src="..\/(.*)" alt="(.*)"/);
           if (partArray) {
             const imageObject = {
+              type: 'image',
               src: partArray[1],
               alt: partArray[2],
             };
             images.push(imageObject);
           }
-
         });
 
         images.forEach((image) => {
-          generateThumbnail(thumbnails, image).then(() => {
-            writeToHtml(templateName, content, images, thumbnails);
+          generateThumbnail(imageSet, image).then(() => {
+            writeToHtml(templateName, content, imageSet);
           });
         });
       });
