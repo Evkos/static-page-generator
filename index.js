7f1f034c3d0cd857da6c79e5850634708b3603cc
evkos <evkos@pm.me>
2020-01-26 02:43:53 +0200

    use event for read/write

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const fs = require('fs');
+const events = require('events');
 const ImageParser = require('./src/ImageParser');
 const ImageCompressor = require('./src/ImageCompressor');
 
 const imageParser = new ImageParser();
 const imageCompressor = new ImageCompressor('top-left', 2);
+const eventEmitter = new events.EventEmitter();
 
 const templatesPath = process.env.TEMPLATES_PATH;
+const publicPath = process.env.PUBLIC_PATH;
 
 const generateThumbnail = async (imageSet, image) => {
   const imageObject = await imageParser.parseImageFileToImageObject(image.src);
@@ -21,63 +24,63 @@ const generateThumbnail = async (imageSet, image) => {
 };
 
 const writeToHtml = (name, content, imageSet) => {
-
   const images = imageSet.reduce((result, item) => {
     if (item.type === 'image') {
       return result.concat(`<img src="../${item.src}" alt="${item.alt}"/>`);
-    } else {
-      return result.concat(`<img src="data:image/bmp;base64,${item.src}" alt="${item.alt}"/>`);
     }
-
+    return result.concat(`<img src="data:image/bmp;base64,${item.src}" alt="${item.alt}"/>`);
   }, '');
 
   const indexData = content.replace(/{{(.*)}}/gs, `${images}`);
 
-  const resultsFolder = 'public';
-
-  if (!fs.existsSync(resultsFolder)) {
-    fs.mkdirSync(resultsFolder);
+  if (!fs.existsSync(publicPath)) {
+    fs.mkdirSync(publicPath);
   }
-  fs.writeFile(`${resultsFolder}/${name}`, indexData, () => {
+  fs.writeFile(`${publicPath}/${name}`, indexData, () => {
   });
 };
 
-const main = () => {
-  fs.readdir(templatesPath, (err, templates) => {
+const getImages = (contentParts) => {
+  const images = [];
+  contentParts.forEach((part) => {
+    const partArray = part.match(/src="..\/(.*)" alt="(.*)"/);
+    if (partArray) {
+      const imageObject = {
+        type: 'image',
+        src: partArray[1],
+        alt: partArray[2],
+      };
+      images.push(imageObject);
+    }
+  });
 
-    templates.forEach((templateName) => {
-      fs.readFile(`${templatesPath}/${templateName}`, (err, buffer) => {
+  return images;
+};
 
-        const content = buffer.toString();
-        const contentParts = content.match(/<(.*)>/g);
+eventEmitter.on('read_template', (templateName) => {
+  fs.readFile(`${templatesPath}/${templateName}`, (err, buffer) => {
 
-        const images = [];
-        const imageSet = [];
+    const content = buffer.toString();
+    const contentParts = content.match(/<(.*)>/g);
 
-        contentParts.forEach((part) => {
-          const partArray = part.match(/src="..\/(.*)" alt="(.*)"/);
-          if (partArray) {
-            const imageObject = {
-              type: 'image',
-              src: partArray[1],
-              alt: partArray[2],
-            };
-            images.push(imageObject);
-          }
-        });
+    const imageSet = [];
+    const images = getImages(contentParts);
 
-        images.forEach((image) => {
-          generateThumbnail(imageSet, image).then(() => {
-            writeToHtml(templateName, content, imageSet);
-          });
-        });
+    images.forEach((image) => {
+      generateThumbnail(imageSet, image).then(() => {
+        writeToHtml(templateName, content, imageSet);
       });
-
     });
   });
-};
+});
 
-main();
+fs.readdir(templatesPath, (err, templates) => {
+  templates.forEach((templateName) => {
+    eventEmitter.emit('read_template', templateName);
+  });
+});
+
+
 
 
 
