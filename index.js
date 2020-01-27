8b3b4f85b571c33758dc8a7c83d2ea94306fe55a
Ievgen Kostiuk <ievgen.kostiuk@altexsoft.com>
2020-01-27 18:28:28 +0200

    foramt code

--- index.js
+++ index.js
@@ -9,26 +9,27 @@ const templatesPath = process.env.TEMPLATES_PATH;
 const eventEmitter = new events.EventEmitter();
 const thumbnailCreator = new ThumbnailCreator();
 const htmlRenderer = new HtmlRenderer();
+const thumbnailsData = [];
 
 fs.readdir(templatesPath, (err, templates) => {
+
   templates.forEach((templateName) => {
-    eventEmitter.emit('read_template', templateName);
+    eventEmitter.emit('read_template', templateName, thumbnailsData);
   });
 });
 
-eventEmitter.on('read_template', (templateName) => {
+eventEmitter.on('read_template', (templateName, thumbnailsData) => {
   fs.readFile(`${templatesPath}/${templateName}`, (err, buffer) => {
 
     const content = buffer.toString();
     const contentParts = content.match(/<(.*)>/g);
-
-    const thumbnailsData = [];
     const initialData = utils.getImages(contentParts);
 
     initialData.forEach((image) => {
-      thumbnailCreator.generateThumbnail(thumbnailsData, image).then(() => {
-        htmlRenderer.renderHtml(templateName, content, thumbnailsData);
-      });
+      thumbnailCreator.generateThumbnail(thumbnailsData, image)
+        .then(() => {
+          htmlRenderer.renderHtml(templateName, content, thumbnailsData);
+        });
     });
   });
 });
